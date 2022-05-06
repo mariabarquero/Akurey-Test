abd4e9f33614f78af2ce8b18b00f1f06a370ce96
mariabarquero <mariabp16@gmail.com>
2022-05-06 11:57:08 -0600

    [+]Pseudocode about how to search in a Btree

---
+++ B_Tree.py
@@ -0,0 +1,19 @@
+#Pseudocode
+"""Input: Recive a node with differents keys, and the key we search
+   Output: If found the key return the key and the position
+           If is a leaf return None becuase we can not continue in this node
+           Else continue search to the differents child of keys
+"""      
+def search_key(key, node):
+    i = 0                                   
+    len_node = len(node)                     
+
+    while i < len_node and key > node[i]:           # Find a key greater than or equal to k
+        i += 1
+
+    if i < len_node and key == node[i]:             # If  key is equal to k
+       return (key,i)
+    elif leaf:                                      # This is a leaf node
+       return None
+    else:
+       return self.search_key(key, tree.child[i])   # Continue with other node
